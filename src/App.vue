18905bab99c7866934ba8f401617abd1fab44a37
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2023-05-16 18:56:33 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-app>
+    <ion-router-outlet :animation="sampleAnimation" />
+                  <!-- 👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆 -->
+                  <!-- this doesn't seem to work -->
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
+import sampleAnimation from "@/utils/Transition";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    IonApp,
+    IonRouterOutlet,
+  },
+  setup() {
+    return { sampleAnimation };
+  },
+});
+</script>
---
+++ src/views/Tabs.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" @click="router.push('/tabs/tab1')">
+          <ion-icon :icon="triangle" />
+          <ion-label>Tab 1</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" @click="router.push('/tabs/tab2')">
+          <ion-icon :icon="ellipse" />
+          <ion-label>Tab 2</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" @click="router.push('/tabs/tab3')">
+          <ion-icon :icon="square" />
+          <ion-label>Tab 3</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import { useRouter } from 'vue-router';
+
+export default {
+  name: 'Tabs',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  setup() {
+    return {
+      router: useRouter(),
+      ellipse, 
+      square, 
+      triangle,
+    }
+  }
+}
+</script>
